4fe4c9ac4485123872651237effb755c5c22b463
John ShaggyTwoDope Jenkins <twodopeshaggy@gmail.com>
2015-08-23 15:37:20 -0700

    add manpage to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(name='define',
       description='Terminal Dictionary',
       long_description=open('README.rst').read(),
-      version='0.4',
-      author='Johnathan "shaggytwodope" Jenkins',
-      author_email='twodopeshaggy@gmail.com',
+      version='0.1',
+      author='SethDusek',
+      author_email='shibe@openmailbox.org',
       url='https://github.com/SethDusek/define',
       data_files=[
       ("share/man/man1", ["define.1"]),
