e32514a9f50e4710f16a6fbe2994f7f5821f15d2
John ShaggyTwoDope Jenkins <twodopeshaggy@gmail.com>
2015-08-23 14:58:42 -0700

    add manpage to setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,11 @@ setup(name='define',
       author='Johnathan "shaggytwodope" Jenkins',
       author_email='twodopeshaggy@gmail.com',
       url='https://github.com/SethDusek/define',
-      scripts=['define.py'],
-      requires=['wordnik'],
+      data_files=[
+      ("share/man/man1", ["define.1"])
+      ],
+      scripts=['define'],
+      requires=['wordnik', 'requests'],
       classifiers=['Intended Audience :: End Users/Desktop',
                    'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 3'],
