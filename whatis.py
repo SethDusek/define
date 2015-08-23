c629c89bae9085c253c48e8cc037813f63738a32
SethDusek <kamalahmad22@hotmail.com>
2015-08-23 12:28:48 +0500

    Added thesaurus + documentation

--- whatis.py
+++ whatis.py
@@ -5,20 +5,31 @@ from sys import argv
 import optparse
 from os import system,remove
 import wget
+import requests
 arg = optparse.OptionParser()
 arg.add_option("-a","--audio",action="store_true",default=False)
+arg.add_option("-t","--thesaurus",action="store_true",default=False)
 opts,rem = arg.parse_args(argv)
 #print(opts)
 audio = opts.audio
+thesaurus = opts.thesaurus
 #print(audio)
 #print(rem)
-key = "1e940957819058fe3ec7c59d43c09504b400110db7faa0509" 
+key = "1e940957819058fe3ec7c59d43c09504b400110db7faa0509"
+tkey = "e415520c671c26518df498d8f4736cac" #thesaurus key
 client = swagger.ApiClient(key,"http://api.wordnik.com/v4")
 client = WordApi.WordApi(client)
 for i in range(1,len(rem)):
     #wget.download(client.getAudio(rem[i])[0].fileUrl)
     print(rem[i].upper() + ": \n" + client.getDefinitions(rem[i])[0].text)
     #"\nEXAMPLE: \n"+ client.getTopExample(rem[i]).text wordnik examples are horrendous so screw them
+    if thesaurus:
+        thes = requests.get("http://words.bighugelabs.com/api/2/%s/%s/json" % (tkey,rem[i])).json()
+        print("SYNONYMS: ")
+        if "noun" in thes:
+            print("\nNouns: %s" % " ".join(thes["noun"]["syn"]))
+        if "verb" in thes:
+            print("Verbs: %s" % " ".join(thes["verb"]["syn"]))
     if audio:
         pass
         ask = raw_input("Would you like to hear audio pronounciation? [Y/N] ")
