5f2db4131a83e1af1e39f5eaf9d40f24652dbd8e
SethDusek <kamalahmad22@hotmail.com>
2015-08-29 18:23:09 +0500

    Fixed audio issue

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 setup(name='define',
       description='Terminal Dictionary',
       long_description=open('README.rst').read(),
-      version='1.51',
+      version='1.52',
       author='SethDusek',
       author_email='shibe@openmailbox.org',
       url='https://github.com/SethDusek/define',
