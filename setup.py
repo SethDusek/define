0d69ed6eff6bb0b9db68f362d7d5feecc6c91cae
SethDusek <kamalahmad22@hotmail.com>
2015-10-01 03:12:55 +0500

    Fixed missing variable rrror

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 setup(name='define',
       description='Terminal Dictionary',
       long_description=open('README.rst').read(),
-      version='1.58',
+      version='1.59',
       author='SethDusek',
       author_email='shibe@openmailbox.org',
       url='https://github.com/SethDusek/define',
