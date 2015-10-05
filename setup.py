427b0ba170495a3b77ff1452f32bc149dfe4c99f
SethDusek <kamalahmad22@hotmail.com>
2015-10-06 00:29:20 +0500

    Added wiktionary

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 setup(name='define',
       description='Terminal Dictionary',
       long_description=open('README.rst').read(),
-      version='1.67',
+      version='1.68',
       author='SethDusek',
       author_email='shibe@openmailbox.org',
       url='https://github.com/SethDusek/define',
