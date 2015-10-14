b4bc5dc53daf195cab41badbee3b198fc4eb225e
SethDusek <kamalahmad22@hotmail.com>
2015-10-14 06:04:14 +0500

    Fixed another typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 setup(name='define',
       description='Terminal Dictionary',
       long_description=open('README.rst').read(),
-      version='1.68',
+      version='1.69',
       author='SethDusek',
       author_email='shibe@openmailbox.org',
       url='https://github.com/SethDusek/define',
