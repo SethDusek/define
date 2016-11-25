ba538d367be989f425a75d889aae14bca7d07f34
Johnathan Jenkins <john@nixheads.co.uk>
2016-11-24 19:11:41 -0800

    minor linting fix

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from version import __version__ as version
 try:
-            from setuptools import setup
+    from setuptools import setup
 except ImportError:
-            from distutils.core import setup
+    from distutils.core import setup
 
 setup(name='define',
       description='Terminal Dictionary',
